b81b5ce5dca750b60449be5187a538b02ca736a4
Marc Tremsal <marc.tremsal@gmail.com>
2022-09-17 10:07:17 -0400

    Make nav options dynamic

--- lib/fly_machines_demo_web/live/lobby_live.ex
+++ lib/fly_machines_demo_web/live/lobby_live.ex
@@ -2,7 +2,6 @@ defmodule FlyMachinesDemoWeb.LobbyLive do
   use FlyMachinesDemoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    # socket = assign(socket, key: value)
     {:ok, socket}
   end
 
