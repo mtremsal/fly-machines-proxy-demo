202d53e94b1ffc9f37311532a28967aca59d8fc4
Marc Tremsal <marc.tremsal@gmail.com>
2022-09-17 10:15:22 -0400

    Make list of instances in nav dynamic

--- lib/fly_machines_demo_web/live/lobby_live.ex
+++ lib/fly_machines_demo_web/live/lobby_live.ex
@@ -1,7 +1,12 @@
 defmodule FlyMachinesDemoWeb.LobbyLive do
   use FlyMachinesDemoWeb, :live_view
 
+  @instances ["abc-123", "def-456", "ghi-789"]
+
   def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:instances, @instances)
     {:ok, socket}
   end
 
